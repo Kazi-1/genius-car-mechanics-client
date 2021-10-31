425d4eafb5154ee00f105dc8966e3fbf4e64f87d
kazi-coder <kazi-coder@github.com>
2021-10-31 09:39:04 +0100

    first commit

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBAFVojRk4pgZwUNOQFqRTR5kwbS_BYaeg",
+    authDomain: "genius-car-mechanics-7d602.firebaseapp.com",
+    projectId: "genius-car-mechanics-7d602",
+    storageBucket: "genius-car-mechanics-7d602.appspot.com",
+    messagingSenderId: "992600036635",
+    appId: "1:992600036635:web:5ee5b9bf26e6272391afbe"
+};
+
+export default firebaseConfig;
